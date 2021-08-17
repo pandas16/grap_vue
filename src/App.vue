d8dbc385553a83caf42d9c884fc37d0deefe62bb
szx <1057095423@qq.com>
2021-08-17 18:07:26 +0800

    调整首页样式（临时）

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="app">
-    <p>这可以看做是导航栏</p>
-    <router-link to="/index">首页</router-link>
-    <router-link to="/userCenter">个人中心</router-link>
-    <router-link to="/demo">演示</router-link>
-    <router-view></router-view>
+    <el-container>
+      <el-header>
+        <el-row type="flex" class="row-bg" justify="space-between">
+          <el-col :span="6">
+            <span>本熊</span>
+          </el-col>
+          <el-col :span="6">
+            <el-menu :default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+              <el-menu-item index="1">
+                <router-link to="/index">首页</router-link>
+              </el-menu-item>
+              <el-submenu index="2">
+                <template slot="title">百宝箱</template>
+                <el-menu-item index="2-1">
+                  <router-link to="/demo">演示</router-link>
+                </el-menu-item>
+              </el-submenu>
+              <el-menu-item index="3">
+                <router-link to="/userCenter">个人中心</router-link>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -16,7 +39,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
